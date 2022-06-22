9bd43d34b050272cdfa60c26b7b66775be5ba14e
Sherzod <shermanjones99@gmail.com>
2022-06-21 20:21:03 -0400

    moved ip functions to its own object

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const ConnectionManager = require("../services/connectionManager");
 const router = express.Router();
+const IpChecker = require("../services/ipChecker");
 
 router.post("/upload", (req, res) => {
-  const originIp = manager.getIp(req);
+  const originIp = IpChecker.getIp(req);
   const originId = req.headers["x-origin-id"];
 
   const file = req.files.file;
--- services/connectionManager.js
+++ services/connectionManager.js
@@ -85,41 +85,6 @@ class ConnectionManager {
     connection.socket.send(message);
   }
 
-  getIp(req) {
-    let ip = "";
-    if (req.headers["x-forwarded-for"]) {
-      ip = req.headers["x-forwarded-for"];
-    } else {
-      ip = req.connection.remoteAddress;
-    }
-
-    // Check if IP is localhost
-    if (ip === "::1" || ip === "::ffff:127.0.0.1") {
-      ip = "127.0.0.1";
-    }
-
-    // Check if IP is private
-    if (this.isIpPrivate(ip)) {
-      ip = "priv";
-    }
-
-    return ip;
-  }
-
-  isIpPrivate(ip) {
-    // Check if ip is in IPv6 format
-    if (ip.indexOf(":") !== -1) {
-      // Convert ip to IPv4 format
-      ip = ip.split(":").pop();
-    }
-    return (
-      ip.startsWith("192.168.") ||
-      ip.startsWith("10.") ||
-      ip.startsWith("172.") ||
-      ip.startsWith("169.")
-    );
-  }
-
   startHeartbeat(connection, origin) {
     this.endHeartbeat(connection);
     const timeout = 30000; // 30 seconds
